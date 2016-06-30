9a13ad8ae2a2dc08f03dc89b18f0ed4a7d16ce6f
Ícaro <mota.icaro@gmail.com>
2016-06-30 17:28:32 -0300

    Implemented the sky following the camera

---
+++ Assets/Scprits/Follow.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * FollowObjectScript
+ * A script that determines when an object will always follow another by a determined offset.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject followed;
+    public Vector3 offset;
+
+    private Vector3 followedPos;
+
+    void Start()
+    {
+        UpdatePosition();
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+        if (followed != null)
+        {
+            followedPos = followed.transform.position;
+            transform.position = followedPos + offset;
+        }
+    }
+}
