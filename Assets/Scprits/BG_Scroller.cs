58010fbe764a8cf34eccd76733697cb28f0899ce
Ícaro <mota.icaro@gmail.com>
2016-06-30 17:12:10 -0300

    Organized the parallax layers.

--- Assets/Scprits/BG_Scroller.cs
+++ Assets/Scprits/BG_Scroller.cs
@@ -2,7 +2,7 @@
 
 public class BG_Scroller : MonoBehaviour
 {
-    public float speed = 2;
+    public float speed = 100;
     private Vector2 direction;
     public bool isLinkedToCamera = false;
 
@@ -10,17 +10,13 @@ public class BG_Scroller : MonoBehaviour
 
     void Update()
     {
-        Vector2 real_speed = speed * 
-            new Vector2(character.GetComponent<Rigidbody2D>().velocity.x,0);
+        Vector2 real_speed = ((100+speed)/100) * 
+            new Vector2(character.GetComponent<Rigidbody2D>().velocity.x,0)
+            - new Vector2(character.GetComponent<Rigidbody2D>().velocity.x, 0);
         Vector3 movement = 
             new Vector3(real_speed.x * -1, real_speed.y * -1, 0);
 
         movement *= Time.deltaTime;
         transform.Translate(movement);
-
-        if (isLinkedToCamera)
-        {
-            //Camera.main.transform.Translate(movement);
-        }
     }
 }
