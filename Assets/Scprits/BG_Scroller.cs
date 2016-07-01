d0f07cb3420a8c7b5624c5563e0a2883d5cf3840
Ícaro <mota.icaro@gmail.com>
2016-06-30 22:40:57 -0300

    Fixed parallax bug

--- Assets/Scprits/BG_Scroller.cs
+++ Assets/Scprits/BG_Scroller.cs
@@ -3,20 +3,29 @@
 public class BG_Scroller : MonoBehaviour
 {
     public float speed = 100;
+    public GameObject cam;
     private Vector2 direction;
     public bool isLinkedToCamera = false;
 
     public GameObject character;
 
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
     void Update()
     {
-        Vector2 real_speed = ((100+speed)/100) * 
-            new Vector2(character.GetComponent<Rigidbody2D>().velocity.x,0)
-            - new Vector2(character.GetComponent<Rigidbody2D>().velocity.x, 0);
-        Vector3 movement = 
-            new Vector3(real_speed.x * -1, real_speed.y * -1, 0);
+        if (cam.GetComponent<Camera>().locked == false)
+        {
+            Vector2 real_speed = ((100 + speed) / 100) *
+                new Vector2(character.GetComponent<Rigidbody2D>().velocity.x, 0)
+                - new Vector2(character.GetComponent<Rigidbody2D>().velocity.x, 0);
+            Vector3 movement =
+                new Vector3(real_speed.x * -1, real_speed.y * -1, 0);
 
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+        }
     }
 }
--- Assets/Scprits/Camera.cs
+++ Assets/Scprits/Camera.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Camera : MonoBehaviour
 {
+    public bool locked = false;
     public Transform character;
     public float leftLimit = -5f, rightLimit = 5.15f, 
                  upperLimit = 0, lowerLimit = -0.025f;
@@ -18,12 +19,29 @@ public class Camera : MonoBehaviour
 
     void Update()
     {
+        locked = false;
         if (character) {
             pos = character.transform.position + new Vector3(size / 2, size / 2, -9);
-            if (pos.x < leftLimit) pos.x = leftLimit;
-            if (pos.x > rightLimit) pos.x = rightLimit;
-            if (pos.y < lowerLimit) pos.y = lowerLimit;
-            if (pos.y > upperLimit) pos.y = upperLimit;
+            if (pos.x <= leftLimit)
+            {
+                pos.x = leftLimit;
+                locked = true;
+            }
+            if (pos.x >= rightLimit)
+            {
+                pos.x = rightLimit;
+                locked = true;
+            }
+            if (pos.y <= lowerLimit)
+            {
+                pos.y = lowerLimit;
+                //locked = true;
+            }
+            if (pos.y >= upperLimit)
+            {
+                pos.y = upperLimit;
+                //locked = true;
+            }
             transform.position = pos;
         }
     }
