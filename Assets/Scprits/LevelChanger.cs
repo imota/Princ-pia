c914bd2eeffc4e5b00867fcdc913b53ec3c2e101
Ícaro <mota.icaro@gmail.com>
2016-07-01 03:08:14 -0300

    We can now change levels. Rako sleeps in the beggining of the game.

--- Assets/Scprits/LevelChanger.cs
+++ Assets/Scprits/LevelChanger.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour {
 
+    public string next_level;
+
 	void OnTriggerEnter2D(Collider2D other)
     {
-
+        SceneManager.LoadScene(next_level);
     }
 }
---
+++ Assets/Scprits/Sleeping.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sleeping : MonoBehaviour {
+
+    public GameObject character;
+
+    void Start()
+    {
+        character.GetComponent<Renderer>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0.2 || Input.GetAxis("Horizontal") < -0.2 ||
+            Input.GetKeyDown("w") || Input.GetKeyDown("joystick button 0"))
+            Move();
+    }
+
+    private void Move()
+    {
+        character.GetComponent<Renderer>().enabled = true;
+        GetComponent<Renderer>().enabled = false;
+    }
+}
