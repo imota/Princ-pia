9236cac13304c4b3b84a97e69775976580a45b32
Ícaro <mota.icaro@gmail.com>
2016-06-30 22:13:36 -0300

    Implemented nightfall Put a lot of animations in the second level. Fixed the art

---
+++ Assets/Scprits/Daytime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Daytime : MonoBehaviour {
+
+    public GameObject character;
+    public float night_time;
+
+	void Update () {
+        Animator animator = GetComponent< Animator>() ;
+        if (character.transform.position.x > night_time)
+        {
+            if (animator.GetBool("Night") == false)
+                animator.SetBool("Night", true);
+        }
+	}
+}
