fa490fd2f711890af643f015482566730cc7fc75
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-07-01 04:02:18 -0300

    First iteration of the enemy, with the move around script.

---
+++ Assets/Scprits/MoveAround.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAround : MonoBehaviour {
+
+    public float speed = 1f;
+
+    private Vector2 origin;
+    private Vector2 checkpoint;
+    private bool moving_to_checkpoint;
+
+    private bool has_rigidbody;
+    private Rigidbody2D rb;
+
+    void Start() {
+        origin = transform.position;
+        checkpoint = transform.FindChild("Checkpoint").position;
+        moving_to_checkpoint = true;
+        rb = GetComponent<Rigidbody2D>();
+        has_rigidbody = rb;
+    }
+
+    void Update() {
+        if (Vector2.Distance((Vector2)transform.position, CurrentDestination()) <= Mathf.Epsilon)
+            moving_to_checkpoint = !moving_to_checkpoint;
+        else {
+            Vector2 dst_point = Vector2.MoveTowards(transform.position,
+                CurrentDestination(), speed*Time.deltaTime);
+            if (has_rigidbody)
+                rb.MovePosition(dst_point);
+            else
+                transform.position = dst_point;
+        }
+    }
+
+    Vector2 CurrentDestination() {
+        return moving_to_checkpoint ? checkpoint : origin;
+    }
+}
