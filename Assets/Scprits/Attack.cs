4dcb92de4c26f33d23c96ad43d9f0971f8be2818
Ícaro <mota.icaro@gmail.com>
2016-07-01 06:05:28 -0300

    God, I´m tired. I did a lot of changes that don´t work trying to implement an attack. But I did change some sprites for the background of the second level.

---
+++ Assets/Scprits/Attack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+    private bool attack;
+    public float time;
+    private Timer timer;
+
+    void Start()
+    {
+        //timer.Set(time);
+    }
+
+	void LateUpdate()
+    {
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("joystick button 2"))
+        {
+            attack = true;
+            //timer.Set(time);
+        }
+        if (true)//timer.IsOver() == false)
+        {
+            //timer.Run();
+            GetComponent<Animator>().SetBool("Idle", false);
+            GetComponent<Animator>().SetBool("Walking", false);
+            GetComponent<Animator>().SetBool("Fall", false);
+            GetComponent<Animator>().SetBool("Jump", false);
+            GetComponent<Animator>().SetBool("Attack", true);
+        }
+    }
+}
